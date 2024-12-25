7598f86a4cf151f16a204b5c9e913d2225bfa5e3
Raj Rajhans <me@rajrajhans.com>
2024-12-25 17:08:02 +0530

    feat: make processing concurrent

--- src/processDirectory.js
+++ src/processDirectory.js
@@ -1,23 +1,40 @@
-const path = require('path')
-const fs = require('fs').promises
+const path = require('path');
+const fs = require('fs').promises;
+const { Sema } = require('async-sema');
 
-const processFile = require('./processFile')
+const processFile = require('./processFile');
 
 const processDirectory = async ({ options, inputPath }) => {
   try {
-    const files = await fs.readdir(inputPath)
+    const sema = new Sema(10);
+
+    const files = await fs.readdir(inputPath);
+    const processPromises = [];
+
     for (const file of files) {
-      const filePath = path.join(inputPath, file)
-      const fileStats = await fs.stat(filePath)
+      const filePath = path.join(inputPath, file);
+      const fileStats = await fs.stat(filePath);
+
       if (fileStats.isFile()) {
-        await processFile({ ...options, filePath })
+        const processWithSemaphore = async () => {
+          try {
+            await sema.acquire();
+            await processFile({ ...options, filePath });
+          } finally {
+            sema.release();
+          }
+        };
+
+        processPromises.push(processWithSemaphore());
       } else if (fileStats.isDirectory() && options.includeSubdirectories) {
-        await processDirectory({ options, inputPath: filePath })
+        await processDirectory({ options, inputPath: filePath });
       }
     }
-  } catch (err) {
-    console.log(err.message)
-  }
-}
 
-module.exports = processDirectory
+    await Promise.all(processPromises);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+module.exports = processDirectory;
